0900b9415f5ec3dcc3b17c3701d993a3a9d1b1fa
debopom-dey <71516417+debopom-dey@users.noreply.github.com>
2023-02-03 14:52:13 +0530

    Update data_conversion.py

--- data_conversion.py
+++ data_conversion.py
@@ -1,29 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv('cleaned_total_stars.csv')
+df = pd.read_csv('final_data.csv')
 print(df.dtypes)
 
-df["Mass"] =df["Mass"].apply(lambda x: x.replace('$', '').replace(',','')).astype('float')
 df["Radius"] =df["Radius"].apply(lambda x: x.replace('$', '').replace(',','')).astype('float')
 
 mass = df['Mass'].tolist()
 radius = df['Radius'].tolist()
 
 
-simass=[]
-for data in mass:
-    mul = data*1.989e+30
-    simass.append(mul)
-
-siradius=[]
-for data in radius:
-    mul2 = data*6.957e+8
-    siradius.append(mul2)
+df["Mass"] =df["Mass"]*1.989e+30
+df["Radius"] =df["Radius"]*6.957e+8
 
 gravity=[]
 G = 6.674e-11
 for index in range(0,len(mass)):
     g= (mass[index]*G)/((radius[index])**2)
     gravity.append(g)
+df["Gravity"]=gravity
 
 df.to_csv("final.csv")
